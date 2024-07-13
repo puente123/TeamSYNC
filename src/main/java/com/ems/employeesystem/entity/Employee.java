cacf2c3784ab7dd985af5f0fb3f1dd00b698b374
Osbaldo Puente Cerda <oxp9670@mavs.uta.edu>
2024-07-12 23:56:10 -0500

    Added JavaDoc plugins and html

--- src/main/java/com/ems/employeesystem/entity/Employee.java
+++ src/main/java/com/ems/employeesystem/entity/Employee.java
@@ -12,25 +12,24 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
- * Employee Class
  * Entity class representing an Employee in the employee management system.
- * This class is mapped to the "employees" table in the MySQL database.
+ * This class is mapped to the "employees" table in the database.
  * 
  * <p>
- * It uses Lombok annotations to generate getters, setters, and constructors.
- * The class is also annotated with JPA annotations
- * to define the table and column mappings in MySQL
+ * It uses Lombok annotations to generate boilerplate code such as getters,
+ * setters, and constructors. The class is also annotated with JPA annotations
+ * to define the table and column mappings.
  * </p>
  * 
  * <p>
  * The fields include:
  * <ul>
- * <li>id: Unique Identifier for the employee. Primary key and auto-generated</li>
- * <li>firstName: Employee's first name mapped to the first_name column in database</li>
- * <li>lastName: Employee's last name mapped to the last_name column in database</li>
- * <li>email: Employee's email address mapped to the email_id column in database, must be unique and not null</li>
+ * <li>id: Primary key, auto-generated</li>
+ * <li>firstName: Employee's first name</li>
+ * <li>lastName: Employee's last name</li>
+ * <li>email: Employee's email address, must be unique and not null</li>
  * </ul>
- * </p>
+ * 
  * 
  * @version 1.0
  * @since 2024-07-12
@@ -44,19 +43,34 @@ import lombok.Setter;
 @Table(name = "employees")
 public class Employee {
 
-
-    
+    /**
+     * The unique identifier for the employee.
+     * This is the primary key and is auto-generated.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * The first name of the employee.
+     * Mapped to the "first_name" column in the database.
+     */
     @Column(name = "first_name")
     private String firstName;
 
+    /**
+     * The last name of the employee.
+     * Mapped to the "last_name" column in the database.
+     */
     @Column(name = "last_name")
     private String lastName;
 
+    /**
+     * The email address of the employee.
+     * Mapped to the "email_id" column in the database.
+     * This field is not nullable and must be unique.
+     */
     @Column(name = "email_id", nullable = false, unique = true)
     private String email;
-
 }
+
